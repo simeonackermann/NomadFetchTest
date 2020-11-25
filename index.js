b56c0d61ed3dee3932cac6177271ef2ff6e88fe4
Simeon Ackermann <s.ackermann@mail.de>
2020-11-25 10:52:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-
-
 console.log('Run...');
+
+var jsonEditor = ace.edit("json-editor")
+jsonEditor.setTheme("ace/theme/github")
+jsonEditor.session.setMode("ace/mode/json")
+jsonEditor.setReadOnly(true);  // false to make it editable
 
 function fetchNomad() {
 
-const nomadUrl = 'https://nomad-lab.eu/prod/rae/api/archive/PACv39BKSFKLtphwxzY-Og/baBkU4yx2wQ1WIQJxuEZhKWq02bS'
+  const nomadUrl = 'https://nomad-lab.eu/prod/rae/api/archive/PACv39BKSFKLtphwxzY-Og/baBkU4yx2wQ1WIQJxuEZhKWq02bS'
 
-fetch(nomadUrl)
-        .then(response => response.json() )
-        .then(data => {
-        	console.log('data', data);
-        })
-        .catch(error => {
-            console.log('error', error);
-        });
+  fetch(nomadUrl)
+    .then(response => response.json() )
+    .then(data => {
+    console.log('data', data);
+    jsonEditor.setValue(JSON.stringify(data, null, '\t'));
+  })
+    .catch(error => {
+    console.log('error', error);
+  });
 
 }
 
